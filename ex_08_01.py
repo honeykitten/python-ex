7c4d18fefcf1606f251e665c58d88c38c92c61ab
gmtae <gmtaey@gmail.com>
2019-12-18 15:14:04 -0800

    add comment

--- ex_08_01.py
+++ ex_08_01.py
@@ -3,7 +3,7 @@ han = open('./files/mbox-short.txt')
 for line in han:
     line = line.rstrip()
     wds = line.split()
-    # guardian in a compound statement
+    # guardian in a compound statement (short circuit evaluation)
     if len(wds) < 3 or wds[0] != 'From' :
         # print('Ignore')
         continue
