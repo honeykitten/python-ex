39537c9a2f6446abfb1e94933c3f6c2c389eacbc
gmtae <gmtaey@gmail.com>
2019-12-18 15:11:36 -0800

    use or

--- ex_08_01.py
+++ ex_08_01.py
@@ -3,10 +3,8 @@ han = open('./files/mbox-short.txt')
 for line in han:
     line = line.rstrip()
     wds = line.split()
-    # guardian
-    if len(wds) < 1 :
-        continue
-    if wds[0] != 'From' :
+    # guardian in a compound statement
+    if len(wds) < 3 or wds[0] != 'From' :
         # print('Ignore')
         continue
     print(wds[2])
